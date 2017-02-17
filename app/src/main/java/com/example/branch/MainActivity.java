8f4eb6cb23e8b763a02d8093a401249ba7586a8b
William Willis <williswm@mail.uc.edu>
2017-02-17 17:56:23 -0500

    Added boolean to NewMethod()

--- app/src/main/java/com/example/branch/MainActivity.java
+++ app/src/main/java/com/example/branch/MainActivity.java
@@ -51,9 +51,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void NewMethod(){
-        int i = 2+2;
+        int i = 3+3;
 
         String test = "Test";
+
+        boolean True = false;
 
     }
 }
