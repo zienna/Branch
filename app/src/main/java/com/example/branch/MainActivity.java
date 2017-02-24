f8cf3ed9a7e98799211b9d13067f7fac4190c4df
William Willis <williswm@mail.uc.edu>
2017-02-24 17:58:05 -0500

    updated mainactivity.java

--- app/src/main/java/com/example/branch/MainActivity.java
+++ app/src/main/java/com/example/branch/MainActivity.java
@@ -52,14 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void doSomeWork(){
 
-        int i = 25;
-        int ii = 80;
+        int i = 60;
+        int ii = 900;
 
         String iString = Integer.toString(i);
         String iiString;
 
-        if(i <= 100){
-            iiString = Integer.toString(ii - 9);
+        if(i >= 100){
+            iiString = Integer.toString(ii * 9);
         }
     }
 }
